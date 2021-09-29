1b2ca338b01bbbed84137959196c452aafc566ce
EC2 Default User <ec2-user@ip-172-31-28-158.eu-west-1.compute.internal>
2021-09-29 15:44:47 +0000

    Initial Commit

---
+++ MatthewSAM_The_Fellowship/src/handlers/put-item.js
@@ -0,0 +1,44 @@
+// Create clients and set shared const values outside of the handler.
+
+// Create a DocumentClient that represents the query to add an item
+const dynamodb = require('aws-sdk/clients/dynamodb');
+const docClient = new dynamodb.DocumentClient();
+
+// Get the DynamoDB table name from environment variables
+const tableName = process.env.TABLE_NAME;
+
+/**
+ * A simple example includes a HTTP post method to add one item to a DynamoDB table.
+ */
+exports.putItemHandler = async (event) => {
+    if (event.httpMethod !== 'POST') {
+        throw new Error(`postMethod only accepts POST method, you tried: ${event.httpMethod} method.`);
+    }
+    // All log statements are written to CloudWatch
+    console.info('received:', event);
+
+    // Get params from the body of the request
+    const body = JSON.parse(event.body)
+    const character_id = body.character_id;
+    const Ring = body.Ring;
+    const Weapon = body.Weapon;
+    const Race = body.Race;
+
+    // Creates a new item, or replaces an old item with a new item
+    // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#put-property
+    var params = {
+        TableName : tableName,
+        Item: { character_id : character_id, Ring: Ring, Weapon : Weapon, Race : Race }
+    };
+
+    const result = await docClient.put(params).promise();
+
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify(body)
+    };
+
+    // All log statements are written to CloudWatch
+    console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
+    return response;
+}
